a7a1843b4dd3d255a2b42f8a0be2dd563bfe2e33
Usachev13 <2893318@gmail.com>
2023-05-27 11:50:56 +0300

    task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HW8 - array chapter 1");
+
+        /* task1 */
+        int [] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        float [] floatNumbers = {1.57f, 7.654f, 9.986f};
+        String [] text = new String[4];
+
     }
 }
